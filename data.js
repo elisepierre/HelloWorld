0598f604d971809c94bc4b05191d691c32fe7195
Elise PIERRE <elise-pierre@outlook.com>
2025-09-25 22:19:10 +0200

    Create continentsData with country information
    Added data for continents and their countries.

---
+++ data.js
@@ -0,0 +1,39 @@
+// Données des continents et pays
+const continentsData = [
+  {
+    name: "Europe",
+    icon: "🌍",
+    countries: [
+      { name: "France", link: "france.html" },
+      { name: "Allemagne", link: "allemagne.html" },
+      { name: "Espagne", link: "espagne.html" }
+    ]
+  },
+  {
+    name: "Afrique",
+    icon: "🌍",
+    countries: [
+      { name: "Maroc", link: "maroc.html" },
+      { name: "Sénégal", link: "senegal.html" },
+      { name: "Afrique du Sud", link: "afrique-du-sud.html" }
+    ]
+  },
+  {
+    name: "Amérique",
+    icon: "🌎",
+    countries: [
+      { name: "Canada", link: "canada.html" },
+      { name: "États-Unis", link: "etats-unis.html" },
+      { name: "Brésil", link: "bresil.html" }
+    ]
+  },
+  {
+    name: "Asie",
+    icon: "🌏",
+    countries: [
+      { name: "Japon", link: "japon.html" },
+      { name: "Chine", link: "chine.html" },
+      { name: "Taïwan", link: "taiwan.html" }
+    ]
+  }
+];
